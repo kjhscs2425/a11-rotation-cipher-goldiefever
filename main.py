6a89aeec0e8e36816e933114e5e5c174dd172e42
goldiefever <mgoldhaberfiebert2028@kehillahstudent.org>
2025-01-15 11:55:58 -0800

    done done

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ def decrypt(secret_message, key):
         if old_position == -1:
             decryption += " "
         else:
-            new_position = old_position + key
+            new_position = old_position - key
             new_position = new_position % len(alphabet)
             new_letter = alphabet[new_position]
             decryption += new_letter
+    print(key)
     return decryption
 
 secret_message = "y qc q iushuj cuiiqwu oek mybb duluh wkuii"
-key = 0
-for i in range(27):
+
+for key in range(26):
     print(decrypt(secret_message, key))
-    key += 1
 # Your task:
 # figure out what key I used to encrypt this message:
 
